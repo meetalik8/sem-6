9649a516b935148517d1919939ee47192798d43e
Meetali Kapse <102135323+meetalik8@users.noreply.github.com>
2024-04-29 18:02:42 +0530

    Create GCDandLCM.dart

---
+++ MAD/GCDandLCM.dart
@@ -0,0 +1,16 @@
+int GCD(int num1, int num2){
+  while(num2!=num1){
+    var temp=num2;
+    num2=num1%num2;
+    num1=num2;
+  }
+  return num1;
+}
+
+int LCM(int num1, int num2){
+  return(num1 * num2) ~/ GCD(num1,num2);
+}
+void main(){
+  int num1=10, num2=8;
+  print("The GCD of ${num1} and ${num2} is ${GCD(num1,num2)} and the LCM is ${LCM(num1,num2)}");
+}
